64847e64f0d8004a17ebb716a682db18c5298b29
kushk4856 <kushk4998@gmail.com>
2024-05-25 14:57:22 +0530

    building singup functionality

---
+++ src/features/authentication/useSignUp.js
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query";
+import { singup } from "../../services/apiAuth";
+import { toast } from "react-toastify";
+
+export function useSingUp() {
+  const { mutate: singUp, isLoading } = useMutation({
+    mutationFn: singup,
+    onSuccess: (user) => {
+      //   console.log(user);
+      toast.success(
+        "Account successfully created!, Please verify the new account from the user's email address"
+      );
+    },
+  });
+
+  return { singUp, isLoading };
+}
