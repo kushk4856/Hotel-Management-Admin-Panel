bdc446dba42288e455d45c365fae90aa56349f24
kushk4856 <kushk4998@gmail.com>
2024-06-09 20:13:31 +0530

    Error Boundary applied

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -11,9 +11,7 @@ function BookingTable() {
 
   if (isLoading) return <Spinner />;
 
-  // console.log(bookings);
-
-  if (!bookings) return <Empty resourceName={"bookings"} />;
+  if (!bookings.length) return <Empty resourceName={"bookings"} />;
 
   return (
     <Menus>
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ToastContainer className="foo" autoClose={3000} />
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    >
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
